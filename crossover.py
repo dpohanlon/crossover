a8a937e9a6949ec4144c2ed9a59940d5ffef89bf
Daniel O'Hanlon <dpohanlon@gmail.com>
2020-07-04 23:11:25 +0100

    Add low/high pass plots.

--- crossover.py
+++ crossover.py
@@ -37,28 +37,30 @@ class Resistor(Component):
 
     def __init__(self, resistance):
         super(Resistor, self).__init__()
-        self.resistance = resistance
+        self.resistance = float(resistance)
 
     def impedence(self, omega):
-        return jax.lax.complex(self.resistance, 0)
+
+        return jax.lax.complex(self.resistance * np.ones((omega.shape)), 0.)
 
 class Inductor(Component):
 
     def __init__(self, inductance):
         super(Inductor, self).__init__()
-        self.inductance = inductance
+        self.inductance = float(inductance)
 
     def impedence(self, omega):
-        return jax.lax.complex(0, omega * self.inductance)
+        return jax.lax.complex(0., omega * self.inductance)
 
 class Capacitor(Component):
 
     def __init__(self, capacitance):
         super(Capacitor, self).__init__()
-        self.capacitance = capacitance
+        self.capacitance = float(capacitance)
 
     def impedence(self, omega):
-        return jax.lax.complex(0, -1./(omega * self.capacitance))
+
+        return jax.lax.complex(0., -1./(omega * self.capacitance))
 
 class Topology(object):
 
@@ -102,11 +104,42 @@ class RLC(Topology):
 class Rx(Topology):
 
     def __init__(self, components):
-        super(Rx, self).__init__()
-        self.components = components
+        super(Rx, self).__init__(components)
 
     def transferFunction(self, omega):
 
-        impedences = [c.impedence(omega) for c in components]
+        impedences = np.array([c.impedence(omega) for c in self.components])
 
-        return impedences[1] / (jax.sum(impedences))
+        return impedences[1, :] / np.sum(impedences, axis = 0)
+
+if __name__ == '__main__':
+
+    # low pass
+
+    # res = Resistor(160.)
+    # cap = Capacitor(1E-9)
+    #
+    # filter = Rx([res, cap])
+
+    # high pass
+
+    res = Resistor(2.4E5)
+    cap = Capacitor(1E-12)
+
+    filter = Rx([cap, res])
+
+    freqs = np.array(np.linspace(10, 10E6, 1000))
+    omegas = 2 * np.pi * freqs
+
+    output = filter.transferFunction(omegas)
+
+    plt.plot(freqs, np.abs(output))
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.savefig('test.pdf')
+    plt.clf()
+
+    plt.plot(freqs, np.angle(output))
+    plt.xscale('log')
+    plt.savefig('testPhase.pdf')
+    plt.clf()
